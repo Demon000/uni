6beefdce9826d4a2a55cfba86966faece696e31a
Demon <demonsingur@gmail.com>
2018-10-30 23:10:12 +0000

    uni: fp: lab4: simplify tests

--- fp/lab4/tests.py
+++ fp/lab4/tests.py
@@ -6,46 +6,45 @@ def run_tests():
 
     # Test addition
     added_expense = expenses.do('add_one', 1, 1, 100)
-    assert added_expense.get_day() == 1
-    assert added_expense.get_type() == 1
-    assert added_expense.get_sum() == 100
+    assert added_expense.matches(1, 1, 100)
 
     added_expense = expenses.do('add_one', 2, 2, 1000)
-    assert added_expense.get_day() == 2
-    assert added_expense.get_type() == 2
-    assert added_expense.get_sum() == 1000
+    assert added_expense.matches(2, 2, 1000)
 
-    # Test update
-    updated_expense = expenses.do('update_one', 1, 1, 500)
-    assert updated_expense.get_sum() == 500
-
-    # Test deletion for day
-    expenses.do('delete', 1)
-    collection = expenses.do('get')
-    assert len(collection) == 1
-    left_expense = collection[0]
-    assert left_expense.get_day() == 2
-    assert left_expense.get_type() == 2
-    assert left_expense.get_sum() == 1000
-
-    # Test deletion for interval
-    added_expense = expenses.do('add_one', 1, 1, 100)
-    assert added_expense.get_day() == 1
-    assert added_expense.get_type() == 1
-    assert added_expense.get_sum() == 100
+    added_expense = expenses.do('add_one', 2, 3, 2000)
+    assert added_expense.matches(2, 3, 2000)
 
     added_expense = expenses.do('add_one', 3, 1, 100)
-    assert added_expense.get_day() == 3
-    assert added_expense.get_type() == 1
-    assert added_expense.get_sum() == 100
+    assert added_expense.matches(3, 1, 100)
 
+    added_expense = expenses.do('add_one', 4, 3, 500)
+    assert added_expense.matches(4, 3, 500)
+
+    try:
+        added_expense = expenses.do('add_one', 2, 2, 2000)
+        assert False
+    except ValueError:
+        pass
+
+    # Test update
+    updated_expense = expenses.do('update_one', 2, 2, 500)
+    assert updated_expense.get_sum() == 500
+
+    try:
+        added_expense = expenses.do('add_one', 5, 1, 2000)
+        assert False
+    except ValueError:
+        pass
+
+    # Test deletion for day
+    expenses.do('delete', 3)
+    collection = expenses.do('get')
+    assert len(collection) == 4
+
+    # Test deletion for interval
     expenses.do('delete', min_day=1, max_day=2)
     collection = expenses.do('get')
     assert len(collection) == 1
-    left_expense = collection[0]
-    assert left_expense.get_day() == 3
-    assert left_expense.get_type() == 1
-    assert left_expense.get_sum() == 100
 
     serialization = expenses.do('serialize')
     print(serialization)
