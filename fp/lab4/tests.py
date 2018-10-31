fae73f7bf8d4c8a55e7853854fb6a7473baae7a6
Demon <demonsingur@gmail.com>
2018-10-31 08:09:00 +0000

    uni: fp: lab4: more detailed tests

--- fp/lab4/tests.py
+++ fp/lab4/tests.py
@@ -4,22 +4,23 @@ from history import History
 def run_tests():
     expenses = History(ExpensesCollection)
 
-    # Test addition
+    # Populate with data
     added_expense = expenses.do('add_one', 1, 1, 100)
-    assert added_expense.matches(1, 1, 100)
+    added_expense = expenses.do('add_one', 1, 2, 100)
+    added_expense = expenses.do('add_one', 1, 3, 100)
+    added_expense = expenses.do('add_one', 2, 1, 2000)
+    added_expense = expenses.do('add_one', 3, 1, 100)
+    added_expense = expenses.do('add_one', 4, 1, 500)
+    added_expense = expenses.do('add_one', 5, 5, 500)
+    added_expense = expenses.do('add_one', 6, 1, 500)
+    added_expense = expenses.do('add_one', 7, 1, 500)
+    added_expense = expenses.do('add_one', 8, 4, 500)
 
+    # Test addition
     added_expense = expenses.do('add_one', 2, 2, 1000)
     assert added_expense.matches(2, 2, 1000)
 
-    added_expense = expenses.do('add_one', 2, 3, 2000)
-    assert added_expense.matches(2, 3, 2000)
-
-    added_expense = expenses.do('add_one', 3, 1, 100)
-    assert added_expense.matches(3, 1, 100)
-
-    added_expense = expenses.do('add_one', 4, 3, 500)
-    assert added_expense.matches(4, 3, 500)
-
+    # Test failed addition
     try:
         added_expense = expenses.do('add_one', 2, 2, 2000)
         assert False
@@ -30,21 +31,53 @@ def run_tests():
     updated_expense = expenses.do('update_one', 2, 2, 500)
     assert updated_expense.get_sum() == 500
 
+    # Test failed update
     try:
-        added_expense = expenses.do('update_one', 5, 1, 2000)
+        added_expense = expenses.do('update_one', 31, 1, 2000)
         assert False
     except ValueError:
         pass
 
     # Test deletion for day
-    expenses.do('delete', 3)
+    deleted_expenses = expenses.do('delete', 3)
+    assert len(deleted_expenses) == 1
+    assert deleted_expenses[0].matches(3, 1, 100)
+    collection = expenses.do('get')
+    assert len(collection) == 10
+
+    # Test deletion for interval
+    deleted_expenses = expenses.do('delete', min_day=1, max_day=2)
+    assert len(deleted_expenses) == 5
+    assert deleted_expenses[0].matches(1, 1, 100)
+    assert deleted_expenses[1].matches(1, 2, 100)
+    assert deleted_expenses[2].matches(1, 3, 100)
+    assert deleted_expenses[3].matches(2, 1, 2000)
+    assert deleted_expenses[4].matches(2, 2, 500)
+    collection = expenses.do('get')
+    assert len(collection) == 5
+
+    # Test deletion for type
+    deleted_expenses = expenses.do('delete', type_=5)
+    assert len(deleted_expenses) == 1
+    assert deleted_expenses[0].matches(5, 5, 500)
     collection = expenses.do('get')
     assert len(collection) == 4
 
-    # Test deletion for interval
-    expenses.do('delete', min_day=1, max_day=2)
-    collection = expenses.do('get')
-    assert len(collection) == 1
+    # Test find larger than a given sum
+    matching_expenses = expenses.do('find', min_sum=200)
+    assert len(matching_expenses) == 4
+    assert matching_expenses[0].matches(4, 1, 500)
+    assert matching_expenses[1].matches(6, 1, 500)
+    assert matching_expenses[2].matches(7, 1, 500)
+    assert matching_expenses[3].matches(8, 4, 500)
+
+    # Test find before a given day and smaller than a given sum
+    matching_expenses = expenses.do('find', max_day=4, max_sum=500)
+    assert matching_expenses[0].matches(4, 1, 500)
+
+    # Test find all expenses of a given expense type
+    matching_expenses = expenses.do('find', type_=4)
+    assert matching_expenses[0].matches(8, 4, 500)
 
 run_tests()
 print('Tests passed.')
