cbafcdf1988f09ed3eb2ea898b32776cf7e437d6
Demon <demonsingur@gmail.com>
2018-11-01 12:15:43 +0000

    Revert "uni: fp: lab4: encode bat in base64"
    This reverts commit d8ba07add0cbe2e6d721a2effb8129ae5a773aa5.

--- fp/lab4/ui.py
+++ fp/lab4/ui.py
@@ -2,7 +2,6 @@ from expenses import ExpensesCollection, Expense
 from history import History
 from menu import Menu, Entry
 from utils import save_as_json, load_from_json
-from base64 import b64decode
 
 def ui_input(message, error, validate_fn=None, type_cast=None):
     while True:
@@ -207,9 +206,54 @@ def ui_exit():
     exit()
 
 def ui_easter_egg():
-    easter_egg_b64 = 'ICAgICMKICAgICAjIwogICAgICMjIwogICAgICAjIyMjCiAgICAgICAjIyMjIwogICAgICAgIyMjIyMjIwogICAgICAgICMjIyMjIyMKICAgICAgICAjIyMjIyMjIwogICAgICAgICMjIyMjIyMjCiAgICAgICAgIyMjIyMjIyMjCiAgICAgICAgIyMjIyMjIyMjIwogICAgICAgIyMjIyMjIyMjIyMjCiAgICAgIyMjIyMjIyMjIyMjIyMKICAgICMjIyMjIyMjIyMjIyMjIyMKICAgICAjIyMjIyMjIyMjIyMjIyMjCiAgICAgICAjIyMjIyMjIyMjIyMjIwogICAgICAgICMjIyMjIyMjIyMjIyMjICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMKICAgICAgICAjIyMjIyMjIyMjIyMjIyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAjIyMjIwogICAgICAgICAjIyMjIyMjIyMjIyMjIyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMjIyMjIwogICAgICAgICAjIyMjIyMjIyMjIyMjIyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjCiAgICAgICAgICMjIyMjIyMjIyMjIyMjIyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMKICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjCiAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMgICAgICAjICAgICAgICAgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIwogICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyAgICAgIyMgICAgIyAgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjICAgIyMjICAgIyMgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyAgIyMjIyMjIyMgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMgICMjIyMjIyMgICAgICAgICAjIyMjIyMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIwogICAgICAgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMjIyMjICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwogICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICAjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICAjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyAgICAgICMjIyMjCiAgICAgICAgICAgICAgICAgICAgICAjIyMgICMjIyMjIyMjIyMjIyMjIyMjIyMgICAgICAgICAgICMjCiAgICAgICAgICAgICAgICAgICAgICAjIyAgICAjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICAgICMgICAgICMjICAjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMgICAgIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAj'
-    easter_egg = '\n'.join(b64decode(easter_egg_b64).decode('utf-8').split('\n'))
-    print(easter_egg)
+    '''
+    #
+     ##
+     ###
+      ####
+       #####
+       #######
+        #######
+        ########
+        ########
+        #########
+        ##########
+       ############
+     ##############
+    ################
+     ################
+       ##############
+        ##############                                              ####
+        ##############                                           #####
+         ##############                                      #######
+         ##############                                 ###########
+         ###############                              #############
+         ################                           ##############
+        #################      #                  ################
+        ##################     ##    #           #################
+       ####################   ###   ##          #################
+            ################  ########          #################
+             ################  #######         ###################
+               #######################       #####################
+                #####################       ###################
+                  ############################################
+                   ###########################################
+                   ##########################################
+                    ########################################
+                    ########################################
+                     ######################################
+                     ######################################
+                      ##########################      #####
+                      ###  ###################           ##
+                      ##    ###############
+                      #     ##  ##########
+                                ##    ###
+                                      ###
+                                      ##
+                                      #
+    '''
+
+    print(ui_easter_egg.__doc__)
 
 def ui_run():
     expenses = History(ExpensesCollection)
