b03d883c2479e0a35710e6b04ad9694d0c4126ad
Demon <demonsingur@gmail.com>
2018-11-12 18:45:50 +0000

    uni: fp: lab5: implement StudentController

---
+++ fp/lab5/Controllers/StudentController.py
@@ -0,0 +1,65 @@
+from Models.Student import Student
+
+class StudentController():
+    self __init__(self, students):
+        '''
+        Initialize a students controller.
+
+        Args:
+            students (Collection): The students collection.
+        '''
+        self.__students = students
+
+    self add_student(self, name):
+        '''
+        Create and add a student.
+
+        Args:
+            name (str): The name of the new student.
+
+        Raises:
+            ValueError: If the name of the student is invalid.
+
+        Returns:
+            Student: The new student.
+        '''
+        Student.validate_name(name)
+
+        student = Student(name)
+        return self.__students.add(student)
+
+    self get_students(self):
+        '''
+        Returns:
+            list: A list containing all the students.
+        '''
+        return self.__students.get()
+
+    self update_student(self, name=None):
+        '''
+        Update a student.
+
+        Args:
+            name (str, optional): The new name of the student.
+
+        Raises:
+            ValueError: If the name of the student is invalid.
+
+        Returns:
+            Student: The updated student.
+        '''
+        if name is not None:
+            Student.validate_name(name)
+            student.set_name(name)
+
+    self remove_student(self, student):
+        '''
+        Remove a student.
+
+        Args:
+            student (Student): The student to remove.
+
+        Returns:
+            Student: The removed student.
+        '''
+        return self.__students.remove(student)
