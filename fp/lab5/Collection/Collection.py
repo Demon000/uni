8933eeae3a37e9e59a99bca5cb388bcd5eb19f84
Demon <demonsingur@gmail.com>
2018-11-12 16:24:59 +0000

    uni: fp: lab5: remove Collection methods that need to be implemented in a Controller

--- fp/lab5/Collection/Collection.py
+++ fp/lab5/Collection/Collection.py
@@ -25,67 +25,6 @@ class Collection():
         items = self.__items[:]
         return items
 
-    def find_matching(self, *args, **kwargs):
-        '''
-        Find the items that match the passed arguments.
-
-        Args:
-            Same arguments as Item.matches().
-
-        Returns:
-            list: A list containing the matching items.
-        '''
-        matching_items = []
-
-        for item in self.__items:
-            if item.matches(*args, **kwargs):
-                matching_items.append(item)
-
-        return matching_items
-
-    def find_one_matching(self, *args, **kwargs):
-        '''
-        Find the item that matches the passed arguments.
-
-        Args:
-            Same arguments as Item.matches().
-
-        Returns:
-            Item: The matching item or None if there is no such item.
-        '''
-        matching_items = self.find_matching(*args, **kwargs)
-
-        if len(matching_items):
-            item = matching_items[0]
-        else:
-            item = None
-
-        return item
-
-    def remove_matching(self, *args, **kwargs):
-        '''
-        Find the items that match the passed arguments and
-        remove them.
-
-        Args:
-            Same arguments as Item.matches().
-
-        Returns:
-            list: A list containing the deleted items.
-        '''
-        removed_items = []
-        left_items = []
-
-        for item in self.__items:
-            if item.matches(*args, **kwargs):
-                removed_items.append(item)
-            else:
-                left_items.append(item)
-
-        self.__items = left_items
-
-        return removed_items
-
     def remove(self, item):
         '''
         Remove the passed item from the collection.
