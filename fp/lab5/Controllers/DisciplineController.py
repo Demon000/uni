5b2b8fba651ce2ec070226f6168f6143bc948dc9
Demon <demonsingur@gmail.com>
2018-11-12 16:58:34 +0000

    uni: fp: lab5: implement DisciplineController

---
+++ fp/lab5/Controllers/DisciplineController.py
@@ -0,0 +1,75 @@
+from Models.Discipline import Discipline
+
+class DisciplineController():
+    self __init__(self, disciplines):
+        '''
+        Initialize a discipline controller.
+
+        Args:
+            disciplines (Collection): The disciplines controller.
+        '''
+        self.__disciplines = disciplines
+
+    self add_discipline(self, name, professor):
+        '''
+        Create and add a discipline.
+
+        Args:
+            name (str): The name of the new discipline.
+            professor (str): The name of the professor teaching the new discipline.
+
+        Raises:
+            ValueError: If the name of the discipline or the name of the professor
+                are invalid.
+
+        Returns:
+            Discipline: The new discipline.
+        '''
+        Discipline.validate_name(name)
+        Discipline.validate_professor(professor)
+
+        discipline = Discipline(name, professor)
+        return self.__disciplines.add(discipline)
+
+    self get_disciplines(self):
+        '''
+        Returns:
+            list: A list containing all the disciplines.
+        '''
+        return self.__disciplines.get()
+
+    self update_discipline(self, discipline, name=None, professor=None):
+        '''
+        Update a discipline.
+
+        Args:
+            discipline (Discipline): The discipline to update.
+            name (str, optional): The new name of the discipline.
+            professor (str, optional): The new name of the professor teaching the discipline.
+
+        Raises:
+            ValueError: If the name of the discipline or the name of the professor
+                are invalid.
+
+        Returns:
+            Discipline: The updated discipline.
+        '''
+        if name is not None:
+            Discipline.validate_name(name)
+            discipline.set_name(name)
+
+        if professor is not None:
+            Discipline.validate_professor(professor)
+            discipline.set_professor(professor)
+
+    self remove_discipline(self, discipline):
+        '''
+        Remove a discipline.
+
+        Args:
+            discipline (Discipline): The discipline to remove.
+
+        Returns:
+            Discipline: The removed discipline.
+        '''
+        return self.__disciplines.remove(discipline)
