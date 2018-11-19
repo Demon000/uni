db8f11564ab6975e13a818cee042ece5c8e64d96
Demon <demonsingur@gmail.com>
2018-11-19 12:55:45 +0000

    uni: fp: lab5: do not validate type in validate_* utility functions

--- fp/lab5/Utils/Utils.py
+++ fp/lab5/Utils/Utils.py
@@ -1,7 +1,4 @@
 def validate_int(value, min_value=None, max_value=None, name='Value'):
-    if type(value) is not int:
-        raise ValueError('{} must be an integer.'.format(name))
-
     if min_value is not None and value < min_value:
         raise ValueError('{} must be larger than {}.'.format(name, min_value - 1))
 
@@ -9,9 +6,6 @@ def validate_int(value, min_value=None, max_value=None, name='Value'):
         raise ValueError('{} must be smaller than {}.'.format(name, max_value + 1))
 
 def validate_str(value, length=None, name='Value'):
-    if type(value) is not str:
-        raise ValueError('{} must be a string.'.format(name))
-
     if length is not None and length > len(value):
         raise ValueError('{} must be at least {} characters long.'.format(name, length))
 
