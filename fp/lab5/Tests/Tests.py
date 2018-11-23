93eeff14dd83f13f9cb83dcc5df6758740bf7b75
Demon <demonsingur@gmail.com>
2018-11-23 16:13:23 +0000

    uni: fp: lab5: merge init methods

--- fp/lab5/Tests/Tests.py
+++ fp/lab5/Tests/Tests.py
@@ -9,30 +9,29 @@ from Validators.Student import StudentValidator
 from Validators.Grade import GradeValidator
 
 class Tests():
-    def init_disciplines(self):
+    def init_empty(self):
         disciplines_collection = Collection()
-        discipline_validator = DisciplineValidator()
-        self.__discipline_service = DisciplineService(disciplines_collection, discipline_validator)
-
-    def init_students(self):
         students_collection = Collection()
-        student_validator = StudentValidator()
-        self.__student_service = StudentService(students_collection, student_validator)
-
-    def init_grades(self):
         grades_collection = Collection()
+
+        discipline_validator = DisciplineValidator()
+        student_validator = StudentValidator()
         grade_validator = GradeValidator()
-        self.__grade_service = GradeService(grades_collection, grade_validator)
+
+        self.__discipline_service = DisciplineService(disciplines_collection, discipline_validator)
+        self.__student_service = StudentService(students_collection, student_validator)
+        self.__grade_service = GradeService(disciplines_collection, students_collection,
+                grades_collection, grade_validator)
 
     def test_add_discipline(self):
-        self.init_disciplines()
+        self.init_empty()
 
         discipline = self.__discipline_service.add_discipline('Sport', 'Moca')
         assert discipline.get_name() == 'Sport'
         assert discipline.get_professor() == 'Moca'
 
     def test_get_disciplines(self):
-        self.init_disciplines()
+        self.init_empty()
 
         self.__discipline_service.add_discipline('Sport', 'Moca')
         disciplines = self.__discipline_service.get_disciplines()
@@ -41,7 +40,7 @@ class Tests():
         assert disciplines[0].get_professor() == 'Moca'
 
     def test_update_discipline(self):
-        self.init_disciplines()
+        self.init_empty()
 
         discipline = self.__discipline_service.add_discipline('Sport', 'Moca')
         self.__discipline_service.update_discipline(discipline, 'Physical training', 'NotMoca')
@@ -49,7 +48,7 @@ class Tests():
         assert discipline.get_professor() == 'NotMoca'
 
     def test_delete_discipline(self):
-        self.init_disciplines()
+        self.init_empty()
 
         discipline = self.__discipline_service.add_discipline('Sport', 'Moca')
         self.__discipline_service.remove_discipline(discipline)
@@ -57,13 +56,13 @@ class Tests():
         assert len(disciplines) == 0
 
     def test_add_student(self):
-        self.init_students()
+        self.init_empty()
 
         student = self.__student_service.add_student('Gigel')
         assert student.get_name() == 'Gigel'
 
     def test_get_students(self):
-        self.init_students()
+        self.init_empty()
 
         self.__student_service.add_student('Gigel')
         students = self.__student_service.get_students()
@@ -71,14 +70,14 @@ class Tests():
         assert students[0].get_name() == 'Gigel'
 
     def test_update_student(self):
-        self.init_students()
+        self.init_empty()
 
         student = self.__student_service.add_student('Gigel')
         self.__student_service.update_student(student, 'Gigi')
         assert student.get_name() == 'Gigi'
 
     def test_delete_student(self):
-        self.init_students()
+        self.init_empty()
 
         student = self.__student_service.add_student('Gigel')
         self.__student_service.remove_student(student)
@@ -87,9 +86,7 @@ class Tests():
         assert len(students) == 0
 
     def test_add_grade(self):
-        self.init_disciplines()
-        self.init_students()
-        self.init_grades()
+        self.init_empty()
 
         discipline = self.__discipline_service.add_discipline('Sport', 'Moca')
         student = self.__student_service.add_student('Gigel')
