331cc27f0a02d5790ace82aa70feb2cc053350bb
Demon <demonsingur@gmail.com>
2019-03-04 07:10:01 +0000

    uni: oop: lab1: fix twin primes

--- oop/lab1/problem.c
+++ oop/lab1/problem.c
@@ -62,62 +62,53 @@ void  run_number_of_0s() {
     printf("The number of zeros in the product of the numbers given is %d.\n", number_of_0s);
 }
 
-
-/*
- * Find the n-th uneven number.
- *
- * Args:
- *      n (int): Obvious.
- *
- * Returns:
- *      int: The the n-th uneven number.
- */
-int get_nth_uneven(int n) {
-    return 2 * n - 1;
-}
-
 struct pair {
     int first;
     int second;
 };
 
 /*
- * Find the n-th pair of cousin numbers.
+ * Find the n-th pair of twin numbers.
  *
  * Args:
  *      n (int): Obvious.
  *
  * Returns:
- *      struct pair*: A pointer to a pair of cousin primes.
+ *      struct pair*: A pointer to a pair of twin primes.
  */
-struct pair* get_nth_cousin_primes_pair(int n) {
+struct pair* get_twin_primes_pair(int n) {
     struct pair* primes;
     primes = malloc(sizeof(struct pair));
 
-    primes->first = get_nth_uneven(n);
-    primes->second = get_nth_uneven(n + 1);
+    if (n == 0) {
+        primes->first = 3;
+        primes->second = 5;
+    } else {
+        primes->first = 6 * n - 1;
+        primes->second = 6 * n + 1;
+    }
 
     return primes;
 }
 
 /*
- * Free the allocated pair of cousin primes.
+ * Free the allocated pair of twin primes.
  *
  * Args:
- *      primes (struct pair*): A pointer to a pair of cousin primes.
+ *      primes (struct pair*): A pointer to a pair of twin primes.
  */
-void free_nth_cousin_primes_pair(struct pair* primes) {
+void free_twin_primes_pair(struct pair* primes) {
     free(primes);
 }
 
 /*
- * Generate the first n pairs of cousin prime numbers.
+ * Generate the first n pairs of twin prime numbers.
  */
 void run_generate_prime_pairs() {
     int n;
     int i;
 
-    printf("Introduce the number of cousin prime pairs to generate: ");
+    printf("Introduce the number of twin prime pairs to generate: ");
     scanf("%d", &n);
 
     if (n < 1) {
@@ -125,13 +116,13 @@ void run_generate_prime_pairs() {
         return;
     }
 
-    printf("The first %d pairs of cousin prime numbers are:\n", n);
-    for (i = 1; i <= n; i++) {
-        struct pair *primes = get_nth_cousin_primes_pair(i);
+    printf("The first %d pairs of twin prime numbers are:\n", n);
+    for (i = 0; i < n; i++) {
+        struct pair *primes = get_twin_primes_pair(i);
 
         printf("%d, %d\n", primes->first, primes->second);
 
-        free_nth_cousin_primes_pair(primes);
+        free_twin_primes_pair(primes);
     }
 }
 
@@ -186,7 +177,7 @@ int main() {
 
     while(1) {
         printf("1. Find the number of 0s in the product of a list of number.\n");
-        printf("2. Find the first n pairs of cousin prime numbers.\n");
+        printf("2. Find the first n pairs of twin prime numbers.\n");
         printf("3. Decompose an even number larger than 2 into the sum of 2 prime numbers.\n");
         printf("0. Exit.\n");
         printf("Enter a command: ");
@@ -207,7 +198,7 @@ int main() {
         /*
          * 15. Determina primele n perechi de numere prime gemene, unde n este un
          *  numar natural nenul dat. Doua numere prime p si q sunt gemene
-         *  daca q-p = 2.
+         *  daca q - p = 2.
          */
         else if (command == 2) {
             run_generate_prime_pairs();
