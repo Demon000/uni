6680aa7cfa41998c9279ef0812e56816774e8237
Demon <demonsingur@gmail.com>
2019-03-15 17:58:12 +0000

    oop: lab2: comment product service

--- oop/lab2/services/ProductService.h
+++ oop/lab2/services/ProductService.h
@@ -18,17 +18,102 @@ typedef enum ProductSortOrder {
     SORT_DESCENDING,
 } ProductSortOrder;
 
+/**
+ * Create a product service.
+ *
+ * @param repository A pointer to the repository to use for product storage.
+ *
+ * @return A pointer to the newly created product service.
+ */
 ProductService* ProductService__create(ProductRepository*);
+
+/**
+ * Destroy a product service.
+ *
+ * @param A pointer to the product service.
+ */
 void ProductService__destroy(ProductService*);
 
+/**
+ * Create a product and add it to the repository.
+ *
+ * @param id The id of the product.
+ * @param price The price of the product.
+ * @param amount The amount of the product.
+ * @param type The type of the product.
+ * @param brand The brand of the product.
+ * @param model The model of the product.
+ *
+ * @return A pointer to the newly created product.
+ */
 Product* ProductService__add_product(ProductService*, ProductId, ProductPrice, ProductAmount, char*, char*, char*);
+
+/**
+ * Find a product by id and remove it from the repository.
+ *
+ * @param id The id of the product.
+ */
 void ProductService__remove_product(ProductService*, ProductId);
+
+/**
+ * Find a product by id and update it.
+ *
+ * @param id The id of the product.
+ * @param price The new price of the product.
+ * @param amount The new amount of the product.
+ *
+ * @return A pointer to the updated product.
+ */
 Product* ProductService__update_product(ProductService*, ProductId, ProductPrice, ProductAmount);
 
+/**
+ * Get all the products sorted by the provided type and order.
+ *
+ * @param service A pointer to the product service.
+ * @param type The type of sort to apply.
+ * @param order The order of the sort.
+ *
+ * @return list A pointer to the list of products.
+ */
 ProductsList* ProductService__get_sorted_products(ProductService*, ProductSortType, ProductSortOrder);
+
+/**
+ * Get all the products from a product service.
+ *
+ * @param service A pointer to the product service.
+ *
+ * @return list A pointer to the list of products.
+ */
 ProductsList* ProductService__get_products(ProductService*);
+
+/**
+ * Get all the products matching the provided brand from a product service.
+ *
+ * @param service A pointer to the product service.
+ * @param brand The brand to look for.
+ *
+ * @return list A pointer to the list of products.
+ */
 ProductsList* ProductService__get_products_by_brand(ProductService*, char*);
+
+/**
+ * Get all the products matching the provided price from a product service.
+ *
+ * @param service A pointer to the product service.
+ * @param price The price to look for.
+ *
+ * @return list A pointer to the list of products.
+ */
 ProductsList* ProductService__get_products_by_price(ProductService*, ProductPrice);
+
+/**
+ * Get all the products matching the provided amount from a product service.
+ *
+ * @param service A pointer to the product service.
+ * @param amount The amount to look for.
+ *
+ * @return list A pointer to the list of products.
+ */
 ProductsList* ProductService__get_products_by_amount(ProductService*, ProductAmount);
 
 #endif // PRODUCT_SERVICE_H
