021ca3afbd408ff272d6be094bfc42217e3677b9
Demon <demonsingur@gmail.com>
2019-03-15 18:11:28 +0000

    oop: lab2: comment vector

--- oop/lab2/utils/Vector.h
+++ oop/lab2/utils/Vector.h
@@ -9,13 +9,62 @@ typedef struct Vector {
     int length;
 } Vector;
 
+/**
+ * Create a vector.
+ *
+ * @return A pointer to the newly created vector.
+ */
 Vector* Vector__create();
+
+/**
+ * Destroy a vector.
+ *
+ * @param vector A pointer to the vector.
+ */
 void Vector__destroy(Vector*);
 
+/**
+ * Get the length of a vector.
+ *
+ * @param vector A pointer to the vector.
+ *
+ * @return The length of the vector.
+ */
 int Vector__get_length(Vector*);
+
+/**
+ * Add an item to the vector.
+ *
+ * @param vector A pointer to the vector.
+ * @param item A pointer to the item.
+ */
 void Vector__add(Vector*, void *);
+
+/**
+ * Set an index of the vector to an item.
+ *
+ * @param vector A pointer to the vector.
+ * @param index An index number.
+ * @param A pointer to the item.
+ */
 void Vector__set(Vector*, int, void *);
+
+/**
+ * Get the item found at an index.
+ *
+ * @param vector A pointer to the vector.
+ * @param index An index number.
+ *
+ * @return A pointer to an item.
+ */
 void* Vector__get(Vector*, int);
+
+/**
+ * Delete the item found at an index.
+ *
+ * @param vector A pointer to the vector.
+ * @param index An index number.
+ */
 void Vector__delete(Vector*, int);
 
 #endif
