c073e9542450ac9aaaba731fcc323bad23c4e118
Demon000 <demonsingur@gmail.com>
2019-04-07 16:35:10 +0300

    oop: lab4: move list implementation to header

--- oop/lab4/list/List.h
+++ oop/lab4/list/List.h
@@ -36,4 +36,71 @@ private:
     int size;
 };
 
+template <typename T>
+Node<T>::Node() {
+    prev = this;
+    next = this;
+}
+
+template <typename T>
+Node<T>::Node(const T& data, Node<T>* prev, Node<T>* next) :
+        data{data}, prev{prev}, next{next} {}
+
+template <typename T>
+List<T>::List() {
+    sentinel = new Node<T>();
+    size = 0;
+}
+
+template <typename T>
+List<T>::~List() {
+    Node<T>* node = sentinel->next;
+    Node<T>* temp;
+    while (node != sentinel) {
+        temp = node->next;
+        delete node;
+        node = temp;
+    }
+
+    delete sentinel;
+}
+
+template <typename T>
+void List<T>::insert_before(Node<T>* next, const T& data) {
+    Node<T>* node = new Node<T>(data, next->prev, next);
+    next->prev->next = node;
+    next->prev = node;
+}
+
+template <typename T>
+void List<T>::push_back(const T& data) {
+    insert_before(sentinel, data);
+}
+
+template <typename T>
+void List<T>::swap_data(Node<T>* first, Node<T>* second) {
+    T temp_data = first->data;
+    first->data = second->data;
+    second->data = temp_data;
+}
+
+template <typename T>
+void List<T>::sort(std::function<bool(const T&, const T&)> cmp) {
+    Node<T>* first = sentinel->next;
+
+    while (first != sentinel) {
+        Node<T>* second = first->next;
+
+        while (second != sentinel) {
+            if (!cmp(first->data, second->data)) {
+                swap_data(first, second);
+            }
+
+            second = second->next;
+        }
+
+        first = first->next;
+    }
+}
+
 #endif // LIST_H
