fd85197993032498371e67e5665b627a2358de9e
Demon000 <demonsingur@gmail.com>
2020-06-02 12:31:54 +0300

    uni: iss: bug_tracker: commonize items

--- iss/bug_tracker/web/vue/BugItem.vue
+++ iss/bug_tracker/web/vue/BugItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bug-item">
+    <div class="bug-item common-item">
         <div class="content" v-if="!isEditing">
             <div class="buttons">
                 <template v-if="status === 'OPEN'">
@@ -103,73 +103,3 @@
         }
     });
 </script>
-
-<style scoped>
-    .bug-item {
-        margin-bottom: 16px;
-
-        border-radius: 4px;
-        background: #1e1e1e;
-
-        position: relative;
-    }
-    .bug-item .content {
-        padding: 8px;
-    }
-    .bug-item .content .title,
-    .bug-item .content .secondary-title,
-    .bug-item .content .description {
-        margin: 0;
-    }
-    .bug-item .content .title {
-        font-size: 1.25rem;
-        font-weight: 500;
-        line-height: 2rem;
-    }
-    .bug-item .content .secondary-title {
-        font-size: 0.875rem;
-        font-weight: 400;
-        line-height: 1.25rem;
-
-        color: rgba(255, 255, 255, 0.6);
-    }
-    .bug-item .content .description {
-        margin: 16px 0 8px 0;
-    }
-    .bug-item .content .buttons {
-        position: absolute;
-        top: 0;
-        right: 0;
-
-        font-size: 18px;
-        padding: 8px;
-    }
-    .bug-item .content .buttons .mdi {
-        display: inline-block;
-        width: 32px;
-        height: 32px;
-        line-height: 24px;
-        text-align: center;
-        padding: 4px;
-
-        border-radius: 50%;
-    }
-    .bug-item .content .buttons .solve {
-        color: #03dac6;
-    }
-    .bug-item .content .buttons .solve:hover {
-        background: rgba(3, 218, 198, 0.06);
-    }
-    .bug-item .content .buttons .delete {
-        color: #cf6679;
-    }
-    .bug-item .content .buttons .delete:hover {
-        background: rgba(207, 102, 121, 0.06);
-    }
-    .bug-item .content .buttons .edit {
-        color: #ffee58;
-    }
-    .bug-item .content .buttons .edit:hover {
-        background: rgba(255, 238, 88, 0.06);
-    }
-</style>
--- iss/bug_tracker/web/vue/UserItem.vue
+++ iss/bug_tracker/web/vue/UserItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="user-item">
+    <div class="user-item common-item">
         <div class="content">
             <p class="title">{{ username }}</p>
             <p class="secondary-title">
@@ -30,31 +30,3 @@
         }
     });
 </script>
-
-<style scoped>
-    .user-item {
-        margin-bottom: 16px;
-
-        border-radius: 4px;
-        background: #1e1e1e;
-    }
-    .user-item .content .title,
-    .user-item .content .secondary-title {
-        margin: 0;
-    }
-    .user-item .content {
-        padding: 8px;
-    }
-    .user-item .content .title {
-        font-size: 1.25rem;
-        font-weight: 500;
-        line-height: 2rem;
-    }
-    .user-item .content .secondary-title {
-        font-size: 0.875rem;
-        font-weight: 400;
-        line-height: 1.25rem;
-
-        color: rgba(255, 255, 255, 0.6);
-    }
-</style>
