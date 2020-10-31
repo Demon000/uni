3e5b4ca97009f83a5decf8a2a1b34daac6e49c19
Demon000 <demonsingur@gmail.com>
2020-10-31 14:02:59 +0200

    uni: pdm: upload lab1 server update

--- pdm/lab1-server/app.py
+++ pdm/lab1-server/app.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_mongoengine import MongoEngine
 
 from api.v1.api_v1 import api as api_v1
 
-db = MongoEngine()
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
-db.init_app(app)
