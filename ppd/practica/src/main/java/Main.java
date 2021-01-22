1cfad8efa530d6d7df4a47d827fa226b93df3232
Demon000 <demonsingur@gmail.com>
2021-01-22 17:17:17 +0200

    ppd: practica

--- ppd/practica/src/main/java/Main.java
+++ ppd/practica/src/main/java/Main.java
@@ -2,43 +2,43 @@ public class Main {
     public static class WorkerThread extends Thread {
         private Parking parking;
         private final ParkingTransactionType transactionType;
+        private int totalCount;
         private final int countPerIteration;
         private final int iterationTime;
 
         public WorkerThread(Parking parking,
                             ParkingTransactionType transactionType,
-                            int countPerIteration, int iterationTime) {
+                            int totalCount,
+                            int countPerIteration,
+                            int iterationTime) {
             this.parking = parking;
             this.transactionType = transactionType;
+            this.totalCount = totalCount;
             this.countPerIteration = countPerIteration;
             this.iterationTime = iterationTime;
         }
 
         @Override
         public void run() {
-            while (true) {
-                for (int i = 0; i < countPerIteration; i++) {
-                    boolean result;
-
+            for (int i = 0; i < totalCount; i++) {
+                for (int j = 0; j < countPerIteration; j++) {
                     if (transactionType == ParkingTransactionType.ENTRY) {
-                        result = parking.entry();
+                        parking.entry();
                     } else if (transactionType == ParkingTransactionType.EXIT) {
-                        result = parking.exit();
+                        parking.exit();
                     } else {
                         throw new RuntimeException("Invalid transaction type");
-                    }
-
-                    if (!result) {
-                        System.out.println("Failed to add transaction");
                     }
                 }
 
                 try {
                     sleep(iterationTime);
                 } catch (InterruptedException e) {
-                    break;
+                    return;
                 }
             }
+
+            parking.endConnection();
         }
     }
 
@@ -63,6 +63,10 @@ public class Main {
                 } catch (InterruptedException e) {
                     break;
                 }
+
+                if (parking.allConnectionsEnded()) {
+                    return;
+                }
             }
         }
     }
@@ -86,6 +90,8 @@ public class Main {
     public static int NO_INITIAL_OCCUPIED_PARKING_PLACES = 25;
     public static int NO_TOTAL_PARKING_PLACES = 100;
 
+    public static int NO_TOTAL_ENTRIES_PER_THREAD = 200;
+    public static int NO_TOTAL_EXITS_PER_THREAD = 275;
     public static int NO_ENTRIES_PER_ITERATION = 1;
     public static int NO_EXITS_PER_ITERATION = 1;
 
@@ -94,19 +100,21 @@ public class Main {
     public static int PRINTER_ITERATION_TIME_MS = 5;
 
     public static void main(String[] args) {
-        Parking parking = new Parking(NO_TOTAL_PARKING_PLACES, NO_INITIAL_OCCUPIED_PARKING_PLACES);
+        Parking parking = new Parking(NO_TOTAL_PARKING_PLACES, NO_INITIAL_OCCUPIED_PARKING_PLACES,
+                NO_WORKER_THREADS_ENTRY + NO_WORKER_THREADS_EXIT);
         PrinterThread printerThread = new PrinterThread(parking, PRINTER_ITERATION_TIME_MS);
         printerThread.start();
 
         for (int i = 0; i < NO_WORKER_THREADS_ENTRY; i++) {
             WorkerThread workerThread = new WorkerThread(parking, ParkingTransactionType.ENTRY,
-                    NO_ENTRIES_PER_ITERATION, ENTRY_ITERATION_TIME_MS);
+                    NO_TOTAL_ENTRIES_PER_THREAD, NO_ENTRIES_PER_ITERATION, ENTRY_ITERATION_TIME_MS);
             workerThread.start();
         }
 
         for (int i = 0; i < NO_WORKER_THREADS_EXIT; i++) {
             WorkerThread workerThread = new WorkerThread(parking, ParkingTransactionType.EXIT,
-                    NO_EXITS_PER_ITERATION, EXIT_ITERATION_TIME_MS);
+                    NO_TOTAL_EXITS_PER_THREAD, NO_EXITS_PER_ITERATION, EXIT_ITERATION_TIME_MS);
             workerThread.start();
-        }    }
+        }
+    }
 }
--- ppd/practica/src/main/java/Parking.java
+++ ppd/practica/src/main/java/Parking.java
@@ -1,40 +1,70 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parking {
     private final List<ParkingTransaction> transactions = Collections.synchronizedList(new ArrayList<>());
     private final AtomicInteger noOccupiedSpaces = new AtomicInteger(0);
+    private final AtomicInteger noConnections;
+    private final BlockingQueue<ParkingTransaction> incomingTransactions;
+    private final ParkingWorker parkingWorker;
     int noTotalSpaces;
 
-    public Parking(int noTotalSpaces, int noOccupiedSpaces) {
+    public Parking(int noTotalSpaces, int noOccupiedSpaces, int noConnections) {
         this.noTotalSpaces = noTotalSpaces;
+        this.noConnections = new AtomicInteger(noConnections);
+
+        incomingTransactions = new LinkedBlockingQueue<>(noTotalSpaces);
 
         while (noOccupiedSpaces > 0) {
             entry();
             noOccupiedSpaces--;
         }
+
+        parkingWorker = new ParkingWorker();
+        parkingWorker.start();
     }
 
-    public boolean entry() {
-        if (noOccupiedSpaces.incrementAndGet() > noTotalSpaces) {
-            noOccupiedSpaces.decrementAndGet();
-            return false;
-        }
+    public class ParkingWorker extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                ParkingTransaction transaction;
 
-        transactions.add(new ParkingTransaction(ParkingTransactionType.ENTRY));
-        return true;
+                try {
+                    transaction = incomingTransactions.take();
+                } catch (InterruptedException e) {
+                    break;
+                }
+
+                if (transaction.transactionType == ParkingTransactionType.ENTRY) {
+                    noOccupiedSpaces.incrementAndGet();
+                } else {
+                    noOccupiedSpaces.decrementAndGet();
+                }
+
+                transactions.add(transaction);
+            }
+        }
     }
 
-    public boolean exit() {
-        if (noOccupiedSpaces.decrementAndGet() < 0) {
-            noOccupiedSpaces.incrementAndGet();
-            return false;
-        }
+    public void endConnection() {
+        noConnections.decrementAndGet();
+    }
 
-        transactions.add(new ParkingTransaction(ParkingTransactionType.EXIT));
-        return true;
+    public boolean allConnectionsEnded() {
+        return noConnections.get() == 0;
+    }
+
+    public void entry() {
+        incomingTransactions.add(new ParkingTransaction(ParkingTransactionType.ENTRY));
+    }
+
+    public void exit() {
+        incomingTransactions.add(new ParkingTransaction(ParkingTransactionType.EXIT));
     }
 
     public AvailableSpacesData getAvailableSpacesData() {
